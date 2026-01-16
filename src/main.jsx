60757431162a439315268ae72921371aee864311
TeresaTome <teresatome@MacBook-Pro-de-Teresa-Tome.local>
2026-01-16 13:44:03 +0100

    Fix: wrap App with BrowserRouter in production

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "leaflet/dist/leaflet.css";
-import AppErrorBoundary from "./components/UI/AppErrorBoundary.jsx";
 
+import "leaflet/dist/leaflet.css";
 import "./styles/leaflet.css";
 import "./styles/app.css";
 import "./index.css";
 
+import AppErrorBoundary from "./components/UI/AppErrorBoundary.jsx";
 import { registerSW } from "virtual:pwa-register";
 
 registerSW({
@@ -23,8 +24,9 @@ registerSW({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppErrorBoundary>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AppErrorBoundary>
   </React.StrictMode>
 );
-
