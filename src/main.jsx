41da67c4b2014cb9cb3a3a623667556b189d3bb0
Pedro Prieto <papv466@gmail.com>
2026-03-04 11:45:17 +0100

    fix: add CartProvider and BrowserRouter to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { CartProvider } from './contexts/CartContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
 
-// SW DESACTIVADO TEMPORALMENTE
 console.log('🚫 Service Worker desactivado')
